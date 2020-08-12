cee39d007ea7d6ef8f708032cecb0058008eeb8c
sinnrrr <dimasoltusyuk@gmail.com>
2020-08-12 17:51:02 +0300

    init

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-mkdir ~/build
-curl -fL https://github.com/cdr/code-server/releases/download/v3.4.1/code-server-3.4.1-linux-amd64.tar.gz \
-  | tar -C ~/build -xz
-ls ~/build
-mv ~/build/code-server-3.4.1-linux-amd64 ~/bin/code-server
-ls ~/bin
+curl -fOL https://github.com/cdr/code-server/releases/download/v3.4.1/code-server_3.4.1_amd64.deb
+sudo dpkg -i code-server_3.4.1_amd64.deb
