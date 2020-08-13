38f0c4a82b1f0dced5b05c5c5851f67249b5302d
sinnrrr <dimasoltusyuk@gmail.com>
2020-08-13 09:46:09 +0300

    init

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 curl -fL https://github.com/cdr/code-server/releases/download/v3.4.1/code-server-3.4.1-linux-amd64.tar.gz \
   | tar -C . -xz
-mv code-server-3.4.1-linux-amd64 server
+mv code-server-3.4.1-linux-amd64 ~/server
