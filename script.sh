87688ef6992cccd206ebae2f67213bcb680be40e
sinnrrr <dimasoltusyuk@gmail.com>
2021-05-22 14:34:03 +0300

    Always getting fresh release from Github API

--- script.sh
+++ script.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-curl -fL https://github.com/cdr/code-server/releases/download/v3.4.1/code-server-3.4.1-linux-amd64.tar.gz | tar -C . -xz
-mv code-server-3.4.1-linux-amd64 server
-ls -l
+curl -s https://api.github.com/repos/cdr/code-server/releases/latest 
+	| grep -E 'browser_download_url' 
+	| grep linux-amd64 
+	| cut -d '"' -f 4
+	| wget -qi - 
+	| tar -C . -xz
+
+mv code-server-*-linux-amd64 server
+
