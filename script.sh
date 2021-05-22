a4cd682a97b29a7456b7ad492c05dc8849d38640
sinnrrr <dimasoltusyuk@gmail.com>
2021-05-22 14:37:04 +0300

    Removing enters

--- script.sh
+++ script.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-curl -s https://api.github.com/repos/cdr/code-server/releases/latest 
-	| grep -E 'browser_download_url' 
-	| grep linux-amd64 
-	| cut -d '"' -f 4
-	| wget -qi - 
-	| tar -C . -xz
-
+curl -s https://api.github.com/repos/cdr/code-server/releases/latest | grep -E 'browser_download_url' | grep linux-amd64 | cut -d '"' -f 4 | wget -qi - | tar -C . -xz
 mv code-server-*-linux-amd64 server
 
