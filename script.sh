a7a3deed1444dd4280e2bf38969a373c39cffb79
sinnrrr <dimasoltusyuk@gmail.com>
2021-05-22 14:37:56 +0300

    Waiting for full download

--- script.sh
+++ script.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-curl -s https://api.github.com/repos/cdr/code-server/releases/latest | grep -E 'browser_download_url' | grep linux-amd64 | cut -d '"' -f 4 | wget -qi - | tar -C . -xz
-mv code-server-*-linux-amd64 server
+curl -s https://api.github.com/repos/cdr/code-server/releases/latest | grep -E 'browser_download_url' | grep linux-amd64 | cut -d '"' -f 4 | wget -qi - | tar -C . -xz && mv code-server-*-linux-amd64 server
 
