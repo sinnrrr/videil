5143b99dec945370a8c6be6162855ba0746db406
sinnrrr <dimasoltusyuk@gmail.com>
2021-05-22 15:10:43 +0300

    Should work

--- script.sh
+++ script.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-curl -s https://api.github.com/repos/cdr/code-server/releases/latest | grep -E 'browser_download_url' | grep linux-amd64 | cut -d '"' -f 4 | wget -qi - -O - | tar -xzC server
+curl -s https://api.github.com/repos/cdr/code-server/releases/latest
+grep -E 'browser_download_url'
+grep linux-amd64 
+cut -d '"' -f 4
+wget -qi - -O - 
+tar -xzC server
